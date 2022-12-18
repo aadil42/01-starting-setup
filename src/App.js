e62de9ec5296d540920cd0de61b829ed6694a0ff
aadil42 <adilkelawala@gmail.com>
2022-12-18 15:27:41 +0530

    created an ExpanceItem component in components directory. Exported it. Imported in the App.js file and rendered it as JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpanceItem from './components/ExpanceItem';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Let's get started! hehe</h2>
+      <ExpanceItem/>
     </div>
   );
 }
---
+++ src/components/ExpanceItem.js
@@ -0,0 +1,7 @@
+function ExpanceItem() {
+    return (
+        <h2>Expance Item.</h2>
+    );
+}
+
+export default ExpanceItem;
