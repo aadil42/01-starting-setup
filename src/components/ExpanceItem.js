2a3496843e4b5ac1c6c39d82732a6a098ff65846
aadil42 <adilkelawala@gmail.com>
2022-12-18 19:45:21 +0530

    passing objects propertis in components, getting the properties in component and displaying all the properties.

--- src/components/ExpanceItem.js
+++ src/components/ExpanceItem.js
@@ -1,19 +1,14 @@
 import './ExpanceItem.css';
 
-function ExpanceItem() {
+function ExpanceItem(props) {
    
-
-
-    const expanceDate = new Date(2022, 12, 18); 
-    const expanceTitle = 'iPhone 13 pro';
-    const expancePrice = '$600.00';
-    // console.log(expanceDate);
+    console.log(props);
     return (
        <div className="expense-item">
-        <div>{expanceDate.toISOString()}</div>
+        <div>{props.date}</div>
         <div className="expense-item__description">
-            <h2>{expanceTitle}</h2>
-            <div className="expense-item__price">{expancePrice}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">{props.amount}</div>
         </div>
        </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
+// import ReactDOM from '../node_modules/react-dom/client';
+
 import './index.css';
 import App from './App';
 
