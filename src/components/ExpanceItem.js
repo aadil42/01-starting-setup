f4564dbe1e25bdc494216b1920915e0ce1364c6b
aadil42 <adilkelawala@gmail.com>
2022-12-19 14:19:23 +0530

    outputting date in a more readable format

--- src/components/ExpanceItem.js
+++ src/components/ExpanceItem.js
@@ -1,11 +1,18 @@
 import './ExpanceItem.css';
 
 function ExpanceItem(props) {
-   
-    console.log(props);
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
+
+    // console.log(props);
     return (
        <div className="expense-item">
-        <div>{props.date}</div>
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">{props.amount}</div>
