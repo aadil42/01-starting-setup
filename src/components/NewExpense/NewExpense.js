382a2b31ad4b0bf32b8dcd3d829de4245292dafe
aadil42 <adilkelawala@gmail.com>
2023-01-15 17:37:01 +0530

    adding addExpense button that hides and displays the form also adding cancel button which can hide the form.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+
+  const [addExpenseButton, setAddExpenseButton] = useState(true);
+
   const saveExpenseDataHandler = (formData) => {
+    // hiding expense add form.
+    setAddExpenseButton(!addExpenseButton);
     props.onAddExpense(formData);
   }
+
+  const toggleExpenseForm = () => {
+    setAddExpenseButton(!addExpenseButton);
+  }
+
+  if(addExpenseButton) {
+    return (
+      <div className='new-expense'>
+        <div className='new-expense__actions'>
+          <button onClick={toggleExpenseForm}>Add New Expense</button>
+        </div>    
+      </div>
+    );
+  }
+
   return (
     <div className='new-expense'>
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
